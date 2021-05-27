6476ae302df7616fd8bc7e6ce92ca22b0d08f50e
jac11 <rootx1982@gmail.com>
2021-05-27 16:56:33 -0400

    Add files via upload

--- Buffer_Helper.py
+++ Buffer_Helper.py
@@ -3,20 +3,41 @@
  
 import time
 import readline 
-
+import sys
 from Buffer_Package             import  Banner 
 from Buffer_Package.__Server    import  SERVERSOCKET
 from Buffer_Package.__Client    import  LISTEN_BIND
 from Buffer_Package.HTTP_LOGIN  import  HTTPLOGIN
 
-W='\033[0m'     
-R='\033[31m'    
-G='\033[0;32m'  
-O='\33[37m'     
-B='\033[34m'    
-P= '\033[35m'   
-Y="\033[1;33m" 
-
+W = '\033[0m'     
+R = '\033[31m'    
+G = '\033[0;32m'  
+O = '\33[37m'     
+B = '\033[34m'    
+P = '\033[35m'   
+Y = '\033[1;33m' 
+try:  
+   if sys.argv[0] and '-c' in sys.argv[1] and  len(sys.argv[1])==2 \
+   and'off' in sys.argv[2] and\
+   len(sys.argv[2])==3 :
+      W = ''     
+      R = ''    
+      G = ''  
+      O = ''     
+      B = ''    
+      P = ''   
+      Y = ''
+   
+   else: 
+       print 'Plsase set color to off'
+       exit()
+except IndexError:
+          try:
+            if sys.argv[1]:
+               print 'Plsase set color to off'
+               exit()  
+          except IndexError:
+               pass
 class Buffer_Helper():
                                          
        def __init__(self):
@@ -46,10 +67,10 @@ class Buffer_Helper():
              print O+"\n[+]"+W+B+" selcet Mode "+W
              print Y+"="*20+W
              print
-             print R+"\n[*]"+W+Y+"'1')"+W+B+ " To Target Server "+W
-             print R+"\n[*]"+W+Y+"'2')"+W+B+ " To Target FTPClient "+W
-             print R+"\n[*]"+W+Y+"'3')"+W+B+ " To Target HTTP Login Page"+W
-             selcet4= str(raw_input(O+"\n[$]"+W+B+"Please Select Mode : "+W))
+             print R+"[*] "+W+Y+ "1-"+W+B+ " To Target Server "+W
+             print R+"[*] "+W+Y+ "2-"+W+B+ " To Target FTPClient "+W
+             print R+"[*] "+W+Y+ "3-"+W+B+ " To Target HTTP Login Page"+W
+             selcet4= str(raw_input(O+"\n[$] "+W+O+"Select Mode Number : "+W))
              if selcet4 == selcet1 and len(selcet4)==1:             
                 run = SERVERSOCKET()                
              elif selcet4 == selcet2 and len(selcet4)==1:             
@@ -58,7 +79,7 @@ class Buffer_Helper():
                  run = HTTPLOGIN()
              else:
                  time.sleep(2)
-                 print Y+" \n[#]"+W+R+"Please Select Mode"+W
+                 print Y+"\n\r\r!---___"+W+R+ "Please Select Mode Number"+W+Y+'___---!'+W
                  return self.main()
          except KeyboardInterrupt:
             print Banner.Banner
